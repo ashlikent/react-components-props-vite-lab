4021e20b6aa387218f7ee42cb53e047078449d84
Ashli Briggs <akent23@gmail.com>
2026-05-06 01:26:05 -0500

    Lab Blog Site UPDATED

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,15 @@
-import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
-import Article from "./Article";
-
-console.log(blogData);
-
+import ArticleList from "./Articlelist";
 function App() {
   return (
     <div className="App">
       <Header name={blogData.name} />
       <About image={blogData.image} about={blogData.about} />
-      {blogData.posts.map((post) => (
-  <Article
-    key={post.id}
-    title={post.title}
-    date={post.date}
-    preview={post.preview}
-  />
-))}
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
-
-
 
 export default App;
