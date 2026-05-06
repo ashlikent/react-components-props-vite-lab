3b5128479c0a4f9787e8e3ef91ada9db4b31c1c3
Ashli Briggs <akent23@gmail.com>
2026-05-06 00:37:57 -0500

    React Blog Site

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import Article from "./Article";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      {blogData.posts.map((post) => (
+  <Article
+    key={post.id}
+    title={post.title}
+    date={post.date}
+    preview={post.preview}
+  />
+))}
     </div>
   );
 }
+
+
 
 export default App;
